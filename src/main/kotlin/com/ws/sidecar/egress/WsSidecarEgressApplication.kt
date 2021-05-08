c4809cbe10e7b51fdc3d034147e0afd49e03d3b0
Zhang Gang <gzhang@thoughtworks.com>
2021-05-08 11:39:42 +0800

    init

--- src/main/kotlin/com/ws/sidecar/egress/WsSidecarEgressApplication.kt
+++ src/main/kotlin/com/ws/sidecar/egress/WsSidecarEgressApplication.kt
@@ -6,6 +6,7 @@ import org.springframework.boot.runApplication
 @SpringBootApplication
 class WsSidecarEgressApplication
 
+
 fun main(args: Array<String>) {
     runApplication<WsSidecarEgressApplication>(*args)
 }

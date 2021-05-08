27346baf53845768057d6ead233b67e774f2f9c5
Zhang Gang <gzhang@thoughtworks.com>
2021-05-08 10:37:15 +0800

    init

---
+++ build.gradle.kts
@@ -0,0 +1,60 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
+
+plugins {
+    id("org.springframework.boot") version "2.4.5"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    kotlin("jvm") version "1.4.32"
+    kotlin("plugin.spring") version "1.4.32"
+    id("org.springframework.experimental.aot") version "0.9.2"
+}
+
+group = "com.ws"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    maven { url = uri("https://repo.spring.io/release") }
+    mavenCentral()
+}
+
+extra["springCloudVersion"] = "2020.0.2"
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.springframework.cloud:spring-cloud-starter-gateway")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "11"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+tasks.withType<BootBuildImage> {
+    builder = "paketobuildpacks/builder:tiny"
+    imageName = "hataketed/${rootProject.name}:${version}"
+    environment = mapOf("BP_NATIVE_IMAGE" to "true")
+}
---
+++ src/main/kotlin/com/ws/sidecar/egress/WsSidecarEgressApplication.kt
@@ -0,0 +1,11 @@
+package com.ws.sidecar.egress
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WsSidecarEgressApplication
+
+fun main(args: Array<String>) {
+    runApplication<WsSidecarEgressApplication>(*args)
+}
